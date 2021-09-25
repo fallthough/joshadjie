702451fe719a905acd3ed196d91a3b53d96b7b45
Gladinette Monzon <gladinette35@gmail.com>
2021-09-25 18:14:17 +0800

    Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -185,20 +185,20 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Paul and Rochelle's Wedding",
+            title: "Josh and Ajie's Wedding",
 
             // Event start date
-            start: new Date('Jun 25, 2021 16:00'),
+            start: new Date('Nov 27, 2021 16:00'),
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Jun 25, 2021 21:00'),
+            end: new Date('Nov 27, 2021 21:00'),
 
             // Event Address
-            address: 'Aquila Crystal Palace, Tagaytay City',
+            address: 'Hacienda Solange, Alfonso rd, Alfonso Cavite',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues contact us: Paul (09088648597) | Rochelle (09190076370)"
+            description: "We can't wait to see you on our big day."
         }
     });
 
